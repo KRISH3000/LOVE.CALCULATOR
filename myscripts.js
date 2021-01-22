fd442d23c2478657bcc68cba91391905d17c05d5
KRISH3000 <71092282+KRISH3000@users.noreply.github.com>
2021-01-22 10:11:12 +0530

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,12 @@
  var x = Math.floor(Math.random() * 100);
+
+if (x > 89) {
+  var y = ("TRUE LOVE");
+} else if (x < 90 ) {
+  var y = ("DON'T LET HIM/HER GO");
+}else if (x < 30) {
+ var y = ("TERA BHI KATEGA");
+}
+else {
+  var y = ("ERROR IN LOVE");
+}
