d811be2a86fdf8ae887bd91851ea8856d20ae96f
KRISH3000 <71092282+KRISH3000@users.noreply.github.com>
2021-01-22 10:15:15 +0530

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -2,10 +2,10 @@
 
 if (x > 89) {
   var y = ("TRUE LOVE");
-} else if (x < 90 ) {
-  var y = ("DON'T LET HIM/HER GO");
-}else if (x < 30) {
+} else if (x < 30) {
  var y = ("TERA BHI KATEGA");
+}else if (x < 90 ) {
+  var y = ("DON'T LET HIM/HER GO");
 }
 else {
   var y = ("ERROR IN LOVE");
